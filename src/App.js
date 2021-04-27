d3aac20e1f423997e9072bad76f1ca4a344e9d90
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-04-27 18:27:13 +0200

    intergrated API
    dictionaryapi.dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 export default function App() {
   let [keyword, setKeyword] = useState("");
+  const apiUrlEndpoint = "https://api.dictionaryapi.dev/api/v2/entries/en_US/";
   
+  function handleApiResponse (response){
+    console.log(response.data);
+  }
   function search (event){
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    axios.get(apiUrlEndpoint + keyword).then(handleApiResponse);
   }
   function handleKeywordChange (event){
     setKeyword(event.target.value);
