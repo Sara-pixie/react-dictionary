60ac162f3307464e8c3b8a363e9d793b776502c3
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-04-29 00:44:09 +0200

    setup a default keyword

--- src/App.js
+++ src/App.js
@@ -3,36 +3,49 @@ import axios from "axios";
 import Results from "./Results";
 import './App.css';
 
-export default function App() {
-  let [keyword, setKeyword] = useState("");
+export default function App(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setloaded] = useState(false);
   const apiUrlEndpoint = "https://api.dictionaryapi.dev/api/v2/entries/en_US/";
   
   function handleApiResponse (response){
     setResults(response.data);
   }
-  function search (event){
-    event.preventDefault();
+  function search(){
     axios.get(apiUrlEndpoint + keyword).then(handleApiResponse);
+  }
+  function handleSubmit (event){
+    event.preventDefault();
+    search();
   }
   function handleKeywordChange (event){
     setKeyword(event.target.value);
   }
+  function load(){
+    setloaded(true);
+    search();
+  }
 
-  return (
-    <div className="App">
-      <h1>Dictionary</h1>
-      <p>What do you want to look up?</p>
-      <form onSubmit={search}>
-        <input type="search" autoFocus={true} placeholder="Search..." onChange={handleKeywordChange} />
-        <button type="submit">Go</button>
-      </form>
-      <Results info={results}/>
-      <footer>
-        Coded by Sara Paranuk and open-sourced on 
-        <a href="https://github.com/Sara-pixie/react-dictionary" target="_blank" rel="noreferrer"> GitHub</a>
-      </footer>
-    </div>
-  );
+  if (loaded){
+    return (
+      <div className="App">
+        <h1>Dictionary</h1>
+        <p>What do you want to look up?</p>
+        <form onSubmit={handleSubmit}>
+          <input type="search" autoFocus={true} placeholder="Search..." onChange={handleKeywordChange} />
+          <button type="submit">Go</button>
+        </form>
+        <Results info={results}/>
+        <footer>
+          Coded by Sara Paranuk and open-sourced on 
+          <a href="https://github.com/Sara-pixie/react-dictionary" target="_blank" rel="noreferrer"> GitHub</a>
+        </footer>
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App defaultKeyword="Hello"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
