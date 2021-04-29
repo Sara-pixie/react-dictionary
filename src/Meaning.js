ecee4f36d12add51d1fefca5be9bdf13ef3fd73c
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-04-29 13:03:49 +0200

    Fixed example display for when there isn't one

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,15 +7,26 @@ export default function Meaning(props){
         <div className="Meaning">
             <h3><i className="far fa-lightbulb"></i> {props.meaning.partOfSpeech}</h3>
             {props.meaning.definitions.map(function (definition, index){
-                return (
-                    <div key={index}>
-                        <p className="definition">
-                            <strong>{index + 1}:</strong> {definition.definition}
-                        </p>
-                        <p className="example"><em>"{definition.example}"</em></p>
-                        <Synonyms synonyms={definition.synonyms} />
-                    </div>
-                );
+                if (definition.example){
+                    return (
+                        <div key={index}>
+                            <p className="definition">
+                                <strong>{index + 1}:</strong> {definition.definition}
+                            </p>
+                            <p className="example"><em>"{definition.example}"</em></p>
+                            <Synonyms synonyms={definition.synonyms} />
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div key={index}>
+                            <p className="definition">
+                                <strong>{index + 1}:</strong> {definition.definition}
+                            </p>
+                            <Synonyms synonyms={definition.synonyms} />
+                        </div>
+                    );
+                }
             })}
         </div>
     );
